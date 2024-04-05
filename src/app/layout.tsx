816dc7fbc1563478470fac728660c464a41bd69a
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-04-05 17:17:00 -0500

    add clerk discord auth instead of nextjs cause it sucks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 // TODO: fix the font it's not working lol
 
 import type { Metadata } from "next";
-import "./globals.css";
 import { TaskbarContextProvider } from "./TaskbarContext";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "D&D Campaign",
@@ -15,13 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <meta name="darkreader-lock" />
-      </head>
-      <TaskbarContextProvider>
-        <body className="font-windowsFont">{children}</body>
-      </TaskbarContextProvider>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <meta name="darkreader-lock" />
+        </head>
+        <TaskbarContextProvider>
+          <body className="font-windowsFont">{children}</body>
+        </TaskbarContextProvider>
+      </html>
+    </ClerkProvider>
   );
 }
